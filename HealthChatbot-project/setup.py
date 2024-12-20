4a0fcde38c0706005d8548614bcaac88afcd1b2b
Meghana Bollam <141807322+meghanareddy3@users.noreply.github.com>
2024-12-20 22:36:44 +0530

    *

---
+++ HealthChatbot-project/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'Rahul Vijay Singh',
+    author_email= 'rahulvijay_singh@srmap.edu.incm',
+    packages= find_packages(),
+    install_requires = []
+
+)
